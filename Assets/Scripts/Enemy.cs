d871581acb3a28b80da97b22523e3f497e386fcc
mmartinezc98 <mariomartinezcarrillo98@gmail.com>
2026-04-30 00:54:28 +0200

    Añadidos personajes y asignadas animaciones. Creado el NavMesh para las superficies y obstaculos. Añadido Script del enemigo. TO DO: ARREGLAR ANIMACIONES DEL GOBLIN Y PENSAR POSIBLES IMPLEMENTACIONES

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,18 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Vector3 min, max;
+    Vector3 destination;
+    bool playerDetected = false;
+
+    private void Start()
     {
-        
+        RandomDestination();
+        StartCoroutine("Patrol");
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator Patrol()
     {
-        
+        while (true)
+        {
+            if (Vector3.Distance(transform.position, destination) < 1.5f)
+            {
+                GetComponent<Animator>().SetFloat("Velocity", 0);
+                yield return new WaitForSeconds(Random.Range(1f, 3f));
+                RandomDestination();
+            }
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    public void RandomDestination()
+    {
+        destination = new Vector3(
+            Random.Range(min.x, max.x), 0,
+            Random.Range(min.z, max.z)
+        );
+        GetComponent<NavMeshAgent>().SetDestination(destination);
+        GetComponent<Animator>().SetFloat("Velocity", 2);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            playerDetected = true;
+            StopCoroutine("Patrol");
+            transform.LookAt(other.transform);
+            GetComponent<NavMeshAgent>().SetDestination(other.transform.position);
+            print("personaje detectado");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            playerDetected = false;
+            RandomDestination();
+            StartCoroutine("Patrol");
+            print("personaje fuera de la detección");
+        }
     }
 }
